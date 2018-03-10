5b3668c645721591bda1b11be4afb3bc7f39457b
Shawn Rice <iamshawnrice@gmail.com>
2018-03-10 18:33:33 -0500

    Expose the express app an an instantiatable function

--- server.js
+++ server.js
@@ -1,37 +1,39 @@
 'use strict';
 
-const bodyParser = require('body-parser');
-const chalk = require('chalk');
-const cors = require('cors');
-const dotenv = require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
+module.exports = () => {
+    const bodyParser = require('body-parser');
+    const chalk = require('chalk');
+    const cors = require('cors');
+    const dotenv = require('dotenv').config();
+    const express = require('express');
+    const mongoose = require('mongoose');
 
-const Note = require('./api/models/notes');
-const isDev = require('./utils').isDev;
+    const Note = require('./api/models/notes');
+    const isDev = require('./utils').isDev;
 
-const app = express();
-const port = process.env.PORT || 3000;
+    const app = express();
+    const port = process.env.PORT || 3000;
 
-// mongoose instance connection url connection
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DB_URI);
+    // mongoose instance connection url connection
+    mongoose.Promise = global.Promise;
+    mongoose.connect(process.env.DB_URI);
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
 
-const corsOptions = {
-    origin: process.env.CORS_WHITELIST
-};
-app.use(cors(corsOptions));
+    const corsOptions = {
+        origin: process.env.CORS_WHITELIST
+    };
+    app.use(cors(corsOptions));
 
-//import routes
-var routes = require('./api/routes/notes'); 
-//register routes
-routes(app);
+    //import routes
+    var routes = require('./api/routes/notes');
+    //register routes
+    routes(app);
 
-app.listen(port);
+    app.listen(port);
 
-if (isDev()) {
-    console.log(chalk.blue(`nizzotes-api server started on: ${port}`));
+    if (isDev()) {
+        console.log(chalk.blue(`nizzotes-api server started on: ${port}`));
+    }
 }
